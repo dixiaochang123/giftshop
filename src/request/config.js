399648c7e01d66f34a249a6cd2b000940e17c222
dixiaochang <dixiaochang@enn.cn>
2022-06-09 11:55:13 +0800

    www.tongtanggift.com

--- src/request/config.js
+++ src/request/config.js
@@ -4,8 +4,8 @@ module.exports = {
       mockUrl: 'http://182.92.2.167:8200',
     },
     'production': {
-      baseUrl: 'http://182.92.2.167:8200',
-      mockUrl: 'http://182.92.2.167:8200'
+      baseUrl: 'www.tongtanggift.com',
+      mockUrl: 'www.tongtanggift.com'
     }
   }
   
