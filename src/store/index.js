2a78d83835a825a6fb11d5db40f936c74af2f177
dixiaochang <dixiaochang@enn.cn>
2022-06-13 00:09:37 +0800

    store add setEdit

--- src/store/index.js
+++ src/store/index.js
@@ -7,19 +7,28 @@ Vue.use(Vuex);     //Vuex大写!!!
 export default new Vuex.Store({
   state: {
     ProductNav: JSON.parse(sessionStorage.getItem('ProductNav')) ? JSON.parse(sessionStorage.getItem('ProductNav')) : null,
+    isEdit:false,
   },
   getters: {
     ProductNav: state => state.ProductNav,
+    isEdit: state => state.isEdit,
   },
   mutations: {
     SETPRODUCTNAV(state, data) {
       sessionStorage.setItem('ProductNav', JSON.stringify(data));
       state.ProductNav = data
     },
+    SETEDIT(state, data) {
+      // sessionStorage.setItem('isEdit', JSON.stringify(data));
+      state.isEdit = data
+    },
   },
   actions: {
     setProductNav({ commit }, data) {
       commit("SETPRODUCTNAV", data)
+    },
+    setEdit({ commit }, data) {
+      commit("SETEDIT", data)
     },
   },
   modules: {
