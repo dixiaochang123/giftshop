ce87f3cf3e73f6a3c05ec7c369a9bb2681afad1d
dixiaochang <dixiaochang@enn.cn>
2022-06-13 15:03:47 +0800

    mockUrl: 'https://post.tongtanggift.com/'

--- src/request/config.js
+++ src/request/config.js
@@ -4,8 +4,8 @@ module.exports = {
       mockUrl: 'http://101.200.36.128:4038',
     },
     'production': {
-      baseUrl: 'http://101.200.36.128:4038',
-      mockUrl: 'http://101.200.36.128:4038'
+      baseUrl: 'https://post.tongtanggift.com/',
+      mockUrl: 'https://post.tongtanggift.com/'
     }
   }
   
