75902d7d5d8672e9aa30c6c5a513190aa755d770
dixiaochang <dixiaochang@enn.cn>
2022-06-13 14:23:23 +0800

    ./devicePixelRatio.js

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,28 @@
 <template>
-    <div id="app">
-        <router-view/>
-    </div>
+  <div id="app">
+    <router-view />
+  </div>
 </template>
 <script>
-import DevicePixelRatio from './devicePixelRatio';
-    export default {
-        name: 'app',
-        components: {},
-         created() {
-            new DevicePixelRatio().init();
-        }
-    }
+import DevicePixelRatio from "./devicePixelRatio.js";
+export default {
+  name: "app",
+  components: {},
+  created() {
+    new DevicePixelRatio().init();
+  },
+  created() {
+    new DevicePixelRatio().init();
+  },
+};
 </script>
 
 <style>
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        height: 100%;
-        color: #2c3e50;
-    }
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  height: 100%;
+  color: #2c3e50;
+}
 </style>
