a84f5ba003b48f357ac2423a407886c6f467f41e
dixiaochang <dixiaochang@enn.cn>
2022-06-13 23:58:28 +0800

    console.log('mainjs',DevicePixelRatio,new DevicePixelRatio())

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 import DevicePixelRatio from "./devicePixelRatio.js";
 new DevicePixelRatio().init();
-cosnole.log('mainjs',DevicePixelRatio)
+console.log('mainjs',DevicePixelRatio,new DevicePixelRatio())
 //element 组件
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css'
