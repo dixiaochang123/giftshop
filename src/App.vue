4c6f201742015bc338f7f536ed6a3da1e3f7281f
dixiaochang <dixiaochang@enn.cn>
2022-06-15 10:58:43 +0800

    window.onresize

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,30 @@ export default {
   mounted() {
     new DevicePixelRatio().init();
     console.log('App mounted',DevicePixelRatio)
+    window.onresize = () => {
+        return (() => {
+          var widths = window.screen.width * window.devicePixelRatio;
+            //页面devicePixelRatio（设备像素比例）变化后，计算页面body标签zoom修改其大小，来抵消devicePixelRatio带来的变化。
+            if(widths==1920 || widths < 1920) {
+                console.log('widths:'+widths)
+                document.getElementsByTagName('body')[0].style.zoom = 1 / window.devicePixelRatio;
+                
+            } else if(widths==2560 || widths < 2560) {
+                console.log('widths:'+widths)
+                document.getElementsByTagName('body')[0].style.zoom = 1;
+            }  else {
+                console.log('widths:'+widths)
+                document.getElementsByTagName('body')[0].style.zoom = 0.7;
+                // document.getElementsByTagName('body')[0].style.transform = 'scale(1)';
+                // document.getElementsByTagName('body')[0].style.transform = 'none';
+                // document.getElementsByTagName('body')[0].style['transform-origin']= 'center';
+                // document.getElementsByTagName('body')[0].style['position']= 'absolute';
+                // document.getElementsByTagName('body')[0].style['width']= '100%';
+                // document.getElementsByTagName('body')[0].style['color']= '100%';
+
+            }
+        })()
+      }
 
   },
 };
